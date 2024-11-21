9afbcfb26c98bf840104ab1563efb60ca6058e99
ahmadhidayat22 <ahmadnurhidayat271@gmail.com>
2024-11-21 17:06:27 +0800

    merge from stagging branch

--- config/Database.js
+++ config/Database.js
@@ -1,9 +1,10 @@
 import  {Sequelize}  from 'sequelize';
-
-const db= new Sequelize('nutrisee', 'root','123',{
-    host: '127.0.0.1',
-    dialect: 'mariadb' // default mysql
+import dotenv from 'dotenv';
+dotenv.config();
+const db= new Sequelize(process.env.DB_NAME , process.env.DB_USER ,process.env.DB_PASSWORD ,{
+    host: process.env.DB_HOST ,
+    dialect: process.env.DB_DIALECT // default mysql
     
-})
+})  
 
 export default db;
