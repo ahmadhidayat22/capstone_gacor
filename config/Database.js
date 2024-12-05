ff6c671c41fa6d88d261cee0bac8ec1543a8123f
ahmadhidayat22 <ahmadnurhidayat271@gmail.com>
2024-12-05 23:40:21 +0800

    update db.js

--- config/Database.js
+++ config/Database.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const db= new Sequelize(process.env.DB_NAME , process.env.DB_USER ,process.env.DB_PASSWORD ,{
     host: process.env.DB_HOST , // localhost
-    dialect: process.env.DB_DIALECT // default mysql
-    
+    port: process.env.DB_PORT, // 3306
+    dialect: process.env.DB_DIALECT, // default mysql
 })  
 
 export default db;
